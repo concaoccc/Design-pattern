3a59f4fbe533829e53ffa2bbd357dccbec49276f
caocongcong <916548030@qq.com>
2018-11-28 15:54:41 +0800

    增加使用系统库

---
+++ src/Char2Weather/UserLibrary/WeatherData.java
@@ -0,0 +1,48 @@
+package Char2Weather.UserLibrary;
+
+import java.util.Observable;
+
+public class WeatherData extends Observable {
+    private float temperature;
+    private float humidity;
+    private float pressure;
+
+    public WeatherData(){
+    }
+
+    public void measurementsChanged(){
+        setChanged();
+        notifyObservers();
+    }
+
+    public void setMeasurements(float temperature, float humidity, float pressure){
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        measurementsChanged();
+    }
+
+    public void setHumidity(float humidity) {
+        this.humidity = humidity;
+    }
+
+    public void setPressure(float pressure) {
+        this.pressure = pressure;
+    }
+
+    public void setTemperature(float temperature) {
+        this.temperature = temperature;
+    }
+
+    public float getHumidity() {
+        return humidity;
+    }
+
+    public float getPressure() {
+        return pressure;
+    }
+
+    public float getTemperature() {
+        return temperature;
+    }
+}
